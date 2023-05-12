85059beb86a3a008717bbe4ffc0eab2ac83d53f4
Arminr7997 <Armin.r7997@gmail.com>
2023-05-12 05:52:56 -0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "https://arminr7997.github.io/crazyfrens/dist",
+  // base: "https://arminr7997.github.io/crazyfrens/dist",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
